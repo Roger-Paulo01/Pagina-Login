c98d1f9083f431c6c9c54f40ef6693e78c9eb1d6
Roger-Paulo01 <rel.cliente.r@gmail.com>
2026-03-09 20:34:19 -0300

    Página de Login

---
+++ linktree/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-amber-800 flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow w-96 h-96">
+        <h1>Hello Word!</h1>
+
+        <button className="bg-red-500 font-bold text-xl hover:animate-spin px-2 py-1">Me aperte!
+        </button>
+      </div>
+    </div>
+    //colocar dentro do bloco div a pagina de login
+  );
+}
+
+export default App;
